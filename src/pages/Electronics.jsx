b7cff3c12beec170d05495dd299284de9529087c
Jamshad <123630640+Jamshad10@users.noreply.github.com>
2023-10-18 18:25:17 +0530

    category settings

--- src/pages/Electronics.jsx
+++ src/pages/Electronics.jsx
@@ -1,18 +1,25 @@
-import { useSelector } from "react-redux"
-
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Electronics() {
-
     const productList = useSelector((state) => state.products.products);
-    console.log('product list', productList);
+    const ItemsList = useSelector((state) => state.items.items);
 
-    const ItemsList = useSelector((state) => state.items.items)
+    const filterProducts = productList.filter((item) => item.category === 'Electronics');
+    const filterItems = ItemsList.filter((item) => item.category === 'Electronics');
 
-    const filterProducts = productList.filter((items) => items.category === 'Electronics')
-    console.log('electronics', filterProducts);
+    const [selectedCategory, setSelectedCategory] = useState(null);
 
-    const filterMobile = ItemsList.filter((items) => items.category === 'Electronics' && items.products === 'Mobile')
-    console.log('mobile', filterMobile);
+    const filterByCategory = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const getFilteredItems = () => {
+        if (!selectedCategory) {
+            return filterItems;
+        }
+        return filterItems.filter((item) => item.products === selectedCategory);
+    };
 
     return (
         <div>
@@ -24,17 +31,12 @@ export default function Electronics() {
                         <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
                             {filterProducts.map((item) => (
                                 <div key={item.id} className="group relative">
-                                    <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                                        <img
-                                            src='https://img.freepik.com/premium-vector/realistic-digital-devices-isometry-set-isometric-illustrations_480270-71.jpg?size=626&ext=jpg&ga=GA1.1.333441761.1696828912&semt=ais'
-                                            alt='img'
-                                            className="h-full w-full object-cover object-center"
-                                        />
-                                    </div>
                                     <p className="text-base font-semibold text-gray-900">{item.products}</p>
                                     <button
                                         type="button"
-                                        className="mt-2 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                        className="mt-2 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                                        onClick={() => filterByCategory(item.products)}
+                                    >
                                         View Products
                                     </button>
                                 </div>
@@ -42,20 +44,20 @@ export default function Electronics() {
                         </div>
                     </div>
                 </div>
+            </div>
+
+            {/* List of Items based on selected category */}
+            <div className="flex">
                 <div>
-                <div>
-                    {filterMobile.map((item) => {
-                        return (
-                            <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg">
+                    <div>
+                        {getFilteredItems().map((item) => (
+                            <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg" key={item._id}>
                                 <li className="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg">{item.item}</li>
                             </ul>
-                        )
-                    })}
-
+                        ))}
+                    </div>
                 </div>
             </div>
-            </div>
-          
         </div>
-    )
+    );
 }
